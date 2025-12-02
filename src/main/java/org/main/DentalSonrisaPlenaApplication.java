1620d15b45e4609f2b923ab8910b3baabad8aa23
AguidoRin <150485648+Aguido-rin@users.noreply.github.com>
2025-12-02 15:16:39 -0500

    Errores Resueltos

--- src/main/java/org/main/DentalSonrisaPlenaApplication.java
+++ src/main/java/org/main/DentalSonrisaPlenaApplication.java
@@ -7,12 +7,18 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"org.main", "org.model", "org.repository", "org.controller"})
+@ComponentScan(basePackages = {
+    "org.main",
+    "org.model",
+    "org.repository",
+    "org.view"
+})
 @EnableJpaRepositories(basePackages = "org.repository")
 @EntityScan(basePackages = "org.model")
 public class DentalSonrisaPlenaApplication {
 
     public static void main(String[] args) {
+        System.setProperty("java.awt.headless", "false"); // Forzar modo gráfico para Swing
         SpringApplication.run(DentalSonrisaPlenaApplication.class, args);
     }
 }
